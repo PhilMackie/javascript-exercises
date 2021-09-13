30e526a7855dae425368c81b0a2a1b9b79c1a27d
PhilMackie <phil.mackie9@gmail.com>
2021-09-13 18:44:14 -0500

    I solved this one - adding my solution to the repo. On to the next one!

--- repeatString.js
+++ repeatString.js
@@ -1,5 +1,18 @@
-const repeatString = function(string, num){
+const number = Math.floor(Math.random() * 1000);
 
+const repeatString = function(string, number){
+
+  if (number < 0){
+    console.log("ERROR");
+    return('ERROR');
+  }console.log(string.repeat(number));
+  return(string.repeat(number));
+  }
+  repeatString('hey', 10); 
+  module.exports = repeatString;
+
+/*
+const repeatString = function(string, num){
 if (num < 0){
   console.log("ERROR");
   return('ERROR');
@@ -8,4 +21,4 @@ return(string.repeat(num));
 }
 repeatString('hey', 10); 
 module.exports = repeatString;
-
+*/
